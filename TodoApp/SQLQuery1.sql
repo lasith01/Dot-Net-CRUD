686c57dcf278e4e9de82bae93498ab8e4baf78c3
Lasith Amanda <lasithamanda1999@example.com>
2025-04-28 10:02:57 +0530

    Added Database and Event Table

---
+++ TodoApp/SQLQuery1.sql
@@ -0,0 +1,11 @@
+﻿INSERT INTO Events (Title, Description, Start, "End", AllDay, CreatedAt)
+VALUES
+('Blazor Conference', 'Conference about Blazor', DATEADD(DAY, 2, GETDATE()), null, 1, GETDATE()),
+('Meet Bill', 'Meeting with Bill Smith', GETDATE(), DATEADD(HOUR, 1, GETDATE()), 0, GETDATE()),
+('Meet John', 'Meeting with John Grant', DATEADD(HOUR, 1, GETDATE()), DATEADD(HOUR, 2, GETDATE()), 0, GETDATE()),
+('Meet Peter', 'Meeting with Peter Harris', DATEADD(HOUR, 2, GETDATE()), DATEADD(HOUR, 3, GETDATE()), 0, GETDATE()),
+('Meet Simon', 'Meeting with Simon Walsh', DATEADD(HOUR, 3, GETDATE()), DATEADD(HOUR, 4, GETDATE()), 0, GETDATE()),
+('Meet Harry', 'Meeting with Harry Russell', DATEADD(DAY, 4, GETDATE()), null, 1, GETDATE()),
+('Meet Angela', 'Meeting with Angela Knox', DATEADD(DAY, 4, GETDATE()), null, 1, GETDATE()),
+('Meet Ava', 'Meeting with Ava Smith', DATEADD(DAY, 4, GETDATE()), null, 1, GETDATE()),
+('ASP.NET Conference', 'Conference about ASP.NET', DATEADD(DAY, 3, GETDATE()), DATEADD(DAY, 7, GETDATE()), 0, GETDATE());
---
+++ TodoApp/WebApi/Services/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected ApplicationDbContext()
+        {
+        }
+
+        public required DbSet<Event> Events { get; set; }
+    }
+}
